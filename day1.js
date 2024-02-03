4dd3ac2c84d92e7d80c360ec0a9c77f4528157df
Emmanuel Fanibi <58971180+efanibi25@users.noreply.github.com>
2024-02-02 19:03:30 -0600

    Create day1.js

---
+++ day1.js
@@ -0,0 +1,10 @@
+function minMax(input){
+    return [Math.max(...input),Math.min(...input)]
+}
+function addUp(target){
+    final=0
+   for (let i = 0; i < target; i++) {
+  final=i+1+final
+} 
+return final
+}
