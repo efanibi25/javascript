1afb987af83a414ca86cd93429fb88d1526be8cc
Emmanuel Fanibi <58971180+efanibi25@users.noreply.github.com>
2024-02-02 19:50:47 -0600

    Update day1.js

--- day1.js
+++ day1.js
@@ -2,9 +2,17 @@ function minMax(input){
     return [Math.max(...input),Math.min(...input)]
 }
 function addUp(target){
-    final=0
-   for (let i = 0; i < target; i++) {
-  final=i+1+final
-} 
-return final
+  Array(target).fill(null).map((_,i)=>i+1).reduce((accumulator,currentValue)=>{
+    return currentValue+accumulator
+})
 }
+
+function shiftToleft(num,count){
+    return num*Math.pow(2,count)
+}
+function largestSwap(val){
+  return val.toString().split("").sort((a,b)=>parseInt(a)-parseInt(b)).reverse().join("")==val
+}
+
+
+
